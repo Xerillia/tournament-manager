a541af935cb270bff860e703941ce6b338353ce3
Xerillia <xerilliasan@gmail.com>
2026-04-12 01:29:16 +0800

    Created tournaments table

---
+++ database/migrations/2026_04_11_140842_create_tournaments_table.php
@@ -0,0 +1,60 @@
+<?php
+
+use App\Gamemode;
+use App\Models\User;
+use App\TournamentStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tournaments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(User::class);
+
+            $table->string('name');
+            $table->tinytext('caption')->nullable();
+
+            $table->enum('gamemode', Gamemode::cases());
+
+            $table->integer('max_rank');
+            $table->integer('min_rank');
+
+            $table->dateTime('start_datetime');
+            $table->dateTime('end_datetime');
+
+            $table->enum('status', TournamentStatus::cases());
+            $table->boolean('automatic_status_update')->default(true);
+
+            $table->string('forum_post')->nullable();
+
+            $table->string('groupchat')->nullable();
+            $table->string('groupchat_platform')->default('Discord');
+
+            $table->string('livestream')->nullable();
+            $table->string('livestream_platform')->default('Twitch');
+
+            $table->string('vod')->nullable();
+            $table->string('vod_platform')->default('YouTube');
+
+            $table->text('rules')->nullable();
+            $table->timestamps();
+
+            $table->unique('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tournaments');
+    }
+};
