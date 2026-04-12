99283f7c699e7fc15bcb6fe56b10d38ecf6a70b0
Xerillia <xerilliasan@gmail.com>
2026-04-12 20:46:11 +0800

    Created players and player_tournament tables migrations

---
+++ database/migrations/2026_04_12_120044_create_player_tournament_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Player;
+use App\Models\Tournament;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('player_tournament', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Player::class);
+            $table->foreignId(Tournament::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('player_tournament');
+    }
+};
