a41358dbe886eedc1d130dbc49e1e13de9e0354c
Xerillia <xerilliasan@gmail.com>
2026-04-12 18:35:56 +0800

    Removed pivot table line from TournamentController

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -35,10 +35,7 @@ class TournamentController extends Controller
         $user_id = ['user_id' => Auth::id()];
 
         // attempt to create tournament
-        $tournament = Tournament::create($request->safe()->merge($user_id)->toArray());
-
-        // update pivot table
-        $tournament->founder()->attach([$user_id]);
+        Tournament::create($request->safe()->merge($user_id)->toArray());
 
         // redirect home
         return redirect()->to(route('landing'));
