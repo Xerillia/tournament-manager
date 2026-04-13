040c30149d8f6d151d9eff798c7b93cf06488d9e
Mads Rurup Nielsen <madsr23@student.sdu.dk>
2026-04-13 17:39:27 +0200

    Added new route

--- routes/tournament.php
+++ routes/tournament.php
@@ -10,6 +10,7 @@ Route::prefix('tournaments')->name('tournaments.')->group(function () {
         Route::post('/', [TournamentController::class, 'store'])->middleware([HandlePrecognitiveRequests::class])->name('store');
         Route::get('/{tournament}', [TournamentController::class, 'show'])->name('show');
         Route::get('/{tournament}/edit', [TournamentController::class, 'edit'])->name('edit');
+        Route::get('/{tournament}/admin', [TournamentController::class, 'admin'])->name('admin');
         Route::put('/{tournament}', [TournamentController::class, 'update'])->middleware([HandlePrecognitiveRequests::class])->name('update');
         Route::delete('/{tournament}', [TournamentController::class, 'destroy'])->name('destroy');
     });
