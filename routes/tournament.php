ee1768e4f993b0350acb10d70db4bf899b46433d
Mads Rurup Nielsen <madsr23@student.sdu.dk>
2026-04-16 00:47:46 +0200

    Fixed naming of duplicate route names #7

--- routes/tournament.php
+++ routes/tournament.php
@@ -11,8 +11,8 @@ Route::prefix('tournaments')->name('tournaments.')->group(function () {
         Route::get('/{tournament}', [TournamentController::class, 'show'])->name('show');
         Route::get('/{tournament}/edit', [TournamentController::class, 'edit'])->name('edit');
         Route::get('/{tournament}/admin', [TournamentController::class, 'admin'])->name('admin');
-        Route::get('/{tournament}/admin/players', [TournamentController::class, 'players'])->name('players');
-        Route::get('/{tournament}/admin/teams', [TournamentController::class, 'teams'])->name('players');
+        Route::get('/{tournament}/admin/players', [TournamentController::class, 'players'])->name('adminPlayers');
+        Route::get('/{tournament}/admin/teams', [TournamentController::class, 'teams'])->name('adminTeams');
         Route::put('/{tournament}', [TournamentController::class, 'update'])->middleware([HandlePrecognitiveRequests::class])->name('update');
         Route::delete('/{tournament}', [TournamentController::class, 'destroy'])->name('destroy');
     });
