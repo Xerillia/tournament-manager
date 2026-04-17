85e6cf4b2ec12225cc670b882f4c9cc6b466a70f
Xerillia <xerilliasan@gmail.com>
2026-04-18 00:50:54 +0800

    Re-added tournament show to auth middleware in routes

--- routes/tournament.php
+++ routes/tournament.php
@@ -6,11 +6,10 @@ use Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('tournaments')->name('tournaments.')->group(function () {
-    Route::get('/{tournament}', [TournamentController::class, 'show'])->name('show');
-
     Route::middleware('auth')->group(function () {
         Route::get('/create', [TournamentController::class, 'create'])->name('create');
         Route::post('/', [TournamentController::class, 'store'])->middleware([HandlePrecognitiveRequests::class])->name('store');
+        Route::get('/{tournament}', [TournamentController::class, 'show'])->name('show');
         Route::get('/{tournament}/edit', [TournamentController::class, 'edit'])->name('edit');
         Route::put('/{tournament}', [TournamentController::class, 'update'])->middleware([HandlePrecognitiveRequests::class])->name('update');
         Route::delete('/{tournament}', [TournamentController::class, 'destroy'])->name('destroy');
