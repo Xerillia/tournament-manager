3871afec2ff8c9817448b53167f1840b3f9fc2a2
Xerillia <xerilliasan@gmail.com>
2026-04-22 23:47:17 +0800

    Remove round unique constraint from mappool #17

--- database/migrations/2026_04_16_101507_add_round_column_to_mappools_table.php
+++ database/migrations/2026_04_16_101507_add_round_column_to_mappools_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('mappools', function (Blueprint $table) {
             $table->string('round')->after('tournament_id');
-            $table->unique('round');
         });
     }
 
