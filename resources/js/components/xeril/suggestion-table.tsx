4774a7f1938f59388ef1e86001666916dd36430f
Xerillia <xerilliasan@gmail.com>
2026-05-03 21:40:20 +0800

    Prototyped tagging deletion in front-end #17

--- resources/js/components/xeril/suggestion-table.tsx
+++ resources/js/components/xeril/suggestion-table.tsx
@@ -4,15 +4,18 @@ import { Suggestion } from '@/types/suggestion';
 import { Tournament } from '@/types/tournament';
 import { router } from '@inertiajs/react';
 import { createColumnHelper, flexRender, getCoreRowModel, useReactTable } from '@tanstack/react-table';
-import { Trash2Icon } from 'lucide-react';
+import { PlusIcon, Trash2Icon, XIcon } from 'lucide-react';
 import { useEffect, useMemo, useState } from 'react';
 import CommentsCell from './comments-cell';
 import { useEcho } from '@laravel/echo-react';
 import { SuggestionComment } from '@/types/comments';
+import { BeatmapTag } from '@/types/beatmaptag';
+import Fuse, { FuseResult } from 'fuse.js/basic';
 
 interface SuggestionTableProps {
     mappool: Mappool;
     tournament: Tournament;
+    tags: BeatmapTag[];
 }
 
 function secondToTime(num: number) {
@@ -28,7 +31,7 @@ function secondToTime(num: number) {
 
 const columnHelper = createColumnHelper<Suggestion>();
 
-export default function SuggestionTable({ mappool, tournament }: SuggestionTableProps) {
+export default function SuggestionTable({ mappool, tournament, tags }: SuggestionTableProps) {
     useEcho('suggestion_comments.mappool.' + mappool.id, 'SuggestionCommentCreated', (e: { suggestionComment: SuggestionComment }) => {
         addNewComment(e);
     });
@@ -285,6 +288,115 @@ export default function SuggestionTable({ mappool, tournament }: SuggestionTable
                 id: 'comments',
                 header: 'Comments',
                 cell: (props) => <CommentsCell props={props} />,
+            }),
+            columnHelper.display({
+                id: 'tags',
+                header: 'Tags',
+                cell: (props) => {
+                    const [suggestionTags, setSuggestionTags] = useState<BeatmapTag[]>(props.row.original.tags);
+                    const [availableTags, setAvailableTags] = useState<BeatmapTag[]>(tags.filter((tag) => !suggestionTags.includes(tag)));
+
+                    const [showPopup, setShowPopup] = useState<boolean>(false);
+
+                    const [searchTerm, setSearchTerm] = useState<string>('');
+                    const [foundTags, setFoundTags] = useState<FuseResult<BeatmapTag>[]>([]);
+
+                    const fuse = new Fuse(availableTags, {
+                        keys: ['id', 'name'],
+                        threshold: 0.5,
+                    });
+
+                    useEffect(() => {
+                        setFoundTags(fuse.search(searchTerm));
+                    }, [searchTerm, availableTags]);
+
+                    useEffect(() => {
+                        setAvailableTags(tags.filter((tag) => !suggestionTags.includes(tag)));
+                    }, [suggestionTags]);
+
+                    function addTag(tag: BeatmapTag) {
+                        setSuggestionTags([...suggestionTags, tag]);
+                    }
+
+                    function removeTag(tag: BeatmapTag) {
+                        setSuggestionTags(suggestionTags.filter((obj) => obj.id !== tag.id));
+                    }
+                    return (
+                        <div className="relative align-middle">
+                            <div className="flex w-32 flex-wrap gap-1 px-2 py-1">
+                                {suggestionTags.map((tag) => (
+                                    <span
+                                        key={tag.id}
+                                        className="flex rounded-full bg-blue-200 px-1 text-xs"
+                                    >
+                                        <p>{tag.name}</p>
+                                        <button
+                                            className="group ml-0.5 hover:cursor-pointer"
+                                            onClick={() => removeTag(tag)}
+                                        >
+                                            <XIcon
+                                                className="size-2.5 rounded-full bg-black group-hover:bg-gray-500"
+                                                color="#fff"
+                                            />
+                                        </button>
+                                    </span>
+                                ))}
+                            </div>
+                            <button
+                                type="button"
+                                className="group p-2 hover:cursor-pointer"
+                                onClick={() => setShowPopup(true)}
+                            >
+                                <PlusIcon
+                                    className="size-5 w-20 rounded-sm bg-green-400 p-0.5 group-hover:bg-green-500"
+                                    color="#000"
+                                />
+                            </button>
+                            {showPopup && (
+                                <>
+                                    <div
+                                        className="fixed top-0 left-0 z-1 h-full w-full"
+                                        onClick={() => setShowPopup(false)}
+                                    />
+                                    <div className="absolute top-0 left-full z-2 rounded-sm border border-black bg-white">
+                                        <input
+                                            type="text"
+                                            name="tag"
+                                            autoComplete="off"
+                                            className="w-full border-b p-2 focus:outline-0"
+                                            placeholder="Search a tag..."
+                                            value={searchTerm}
+                                            onChange={(e) => setSearchTerm(e.target.value)}
+                                        />
+                                        <div className="max-h-80 min-h-40 w-80 flex-col overflow-y-auto">
+                                            {!searchTerm &&
+                                                availableTags.map((tag) => (
+                                                    <button
+                                                        key={tag.id}
+                                                        className="w-full border px-2 py-1 hover:cursor-pointer hover:bg-black/10"
+                                                        onClick={() => addTag(tag)}
+                                                    >
+                                                        {tag.name}
+                                                    </button>
+                                                ))}
+                                            {searchTerm &&
+                                                foundTags.map((tag) => (
+                                                    <button
+                                                        key={tag.item.id}
+                                                        className="w-full border px-2 py-1 hover:cursor-pointer hover:bg-black/10"
+                                                        onClick={() => addTag(tag.item)}
+                                                    >
+                                                        <p>{tag.item.name}</p>
+                                                    </button>
+                                                ))}
+                                            {searchTerm && foundTags.length === 0 && <p className="leading-40">No tags matched!</p>}
+                                        </div>
+                                    </div>
+                                </>
+                            )}
+                        </div>
+                    );
+                },
             }),
             columnHelper.accessor((row) => row.beatmap, {
                 id: 'banner',
