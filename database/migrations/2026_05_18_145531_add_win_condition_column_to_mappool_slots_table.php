9d86fc3e3dc5c5f267ff450ac4294d34deefd01d
Xerillia <xerilliasan@gmail.com>
2026-05-19 00:41:39 +0800

    Patched win condition column default value to use php enum #33

--- database/migrations/2026_05_18_145531_add_win_condition_column_to_mappool_slots_table.php
+++ database/migrations/2026_05_18_145531_add_win_condition_column_to_mappool_slots_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\WinCondition;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -12,7 +13,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('mappool_slots', function (Blueprint $table) {
-            $table->string('win_condition')->after('slot')->default('score_v2');
+            $table->string('win_condition')->after('slot')->default(WinCondition::SCORE_V2);
         });
     }
 
