9ac4e276a57d804f038c739070bf4f095e7b58f1
Xerillia <xerilliasan@gmail.com>
2026-05-18 23:34:14 +0800

    Update Slot type #33

--- app/Enums/WinCondition.php
+++ app/Enums/WinCondition.php
@@ -4,9 +4,20 @@ namespace App\Enums;
 
 enum WinCondition: string
 {
-    case SCORE_V2 = 'Score V2';
-    case SCORE_V1 = 'Score V1';
-    case ACCURACY = 'Accuracy';
-    case COMBO = 'Combo';
-    case LEAST_MISS = 'Least Miss';
+    case SCORE_V2 = 'score_v2';
+    case SCORE_V1 = 'score_v1';
+    case ACCURACY = 'accuracy';
+    case COMBO = 'combo';
+    case LEAST_MISS = 'least_miss';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::SCORE_V2 => 'Score V2',
+            self::SCORE_V1 => 'Score V1',
+            self::ACCURACY => 'Accuracy',
+            self::COMBO => 'Combo',
+            self::LEAST_MISS => 'Least Miss',
+        };
+    }
 }
