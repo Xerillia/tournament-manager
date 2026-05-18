e7159c66ed8269d4bb23c7b3c5b4d23d4c8c47ab
Xerillia <xerilliasan@gmail.com>
2026-05-19 00:26:22 +0800

    Added form logic #33

--- resources/js/components/xeril/win-condition.tsx
+++ resources/js/components/xeril/win-condition.tsx
@@ -1,4 +1,5 @@
 import { WinConditionUtils } from '@/enums';
+import { setWinCondition } from '@/routes/slots';
 import { Slot } from '@/types/mappools';
 import { Form } from '@inertiajs/react';
 import { useState } from 'react';
@@ -19,43 +20,51 @@ export default function WinCondition({ slot }: WinConditionProps) {
                 {WinConditionUtils.label(slot.win_condition)}
             </button>
             {showModal && (
-                <Form method="POST">
-                    <div
-                        className="fixed inset-0 z-4 h-screen w-screen bg-black/20"
-                        onClick={() => setShowModal(false)}
-                    />
-                    <div className="absolute top-1/2 left-1/2 z-5 -translate-1/2 bg-white">
-                        <h1 className="mx-4 mt-4 border-b-2 text-2xl font-semibold">Slot {slot.slot} Win Condition</h1>
-                        <select
-                            name="win_condition"
-                            className="my-6 p-2 focus:outline-0"
-                            defaultValue={slot.win_condition}
-                        >
-                            {WinConditionUtils.options().map((option) => (
-                                <option
-                                    key={option}
-                                    value={option}
-                                >
-                                    {WinConditionUtils.label(option)}
-                                </option>
-                            ))}
-                        </select>
-                        <div className="flex">
-                            <button
-                                type="submit"
-                                className="flex-1 cursor-pointer bg-green-200 p-2 hover:bg-green-300"
-                            >
-                                Save
-                            </button>
-                            <button
-                                type="button"
-                                className="flex-1 cursor-pointer bg-red-200 p-2 hover:bg-red-300"
+                <Form
+                    action={setWinCondition(slot)}
+                    onSuccess={() => setShowModal(false)}
+                >
+                    {({ processing }) => (
+                        <>
+                            <div
+                                className="fixed inset-0 z-4 h-screen w-screen bg-black/20"
                                 onClick={() => setShowModal(false)}
-                            >
-                                Cancel
-                            </button>
-                        </div>
-                    </div>
+                            />
+                            <div className="absolute top-1/2 left-1/2 z-5 -translate-1/2 bg-white">
+                                <h1 className="mx-4 mt-4 border-b-2 text-2xl font-semibold">Slot {slot.slot} Win Condition</h1>
+                                <select
+                                    name="win_condition"
+                                    className="my-6 p-2 focus:outline-0"
+                                    defaultValue={slot.win_condition}
+                                >
+                                    {WinConditionUtils.options().map((option) => (
+                                        <option
+                                            key={option}
+                                            value={option}
+                                        >
+                                            {WinConditionUtils.label(option)}
+                                        </option>
+                                    ))}
+                                </select>
+                                <div className="flex">
+                                    <button
+                                        type="submit"
+                                        className="flex-1 cursor-pointer bg-green-200 p-2 hover:bg-green-300 disabled:cursor-not-allowed"
+                                        disabled={processing}
+                                    >
+                                        Save
+                                    </button>
+                                    <button
+                                        type="button"
+                                        className="flex-1 cursor-pointer bg-red-200 p-2 hover:bg-red-300"
+                                        onClick={() => setShowModal(false)}
+                                    >
+                                        Cancel
+                                    </button>
+                                </div>
+                            </div>
+                        </>
+                    )}
                 </Form>
             )}
         </>
