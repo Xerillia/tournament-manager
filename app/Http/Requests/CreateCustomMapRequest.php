db7e87861c70ca1a3ed15447607771fd82c6367a
Xerillia <xerilliasan@gmail.com>
2026-05-24 22:46:11 +0800

    Created form request for creating custom map #42

---
+++ app/Http/Requests/CreateCustomMapRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\CustomMapStatus;
+use App\Rules\ValidMods;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CreateCustomMapRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, array<mixed>>
+     */
+    public function rules(): array
+    {
+        return [
+            'mapper' => ['required', 'string'],
+            'beatmap_url' => ['required', 'url'],
+            'beatmap_name' => ['required', 'string'],
+            'mods' => ['required', new ValidMods],
+            'status' => ['required', Rule::enum(CustomMapStatus::class)],
+            'bpm' => ['required', 'numeric'],
+            'cs' => ['required', 'numeric', 'min:0'],
+            'ar' => ['required', 'numeric', 'min:0'],
+            'od' => ['required', 'numeric', 'min:0'],
+        ];
+    }
+}
