027733439cc5075dd58b2f2d519091aa9a07e6b6
Xerillia <xerilliasan@gmail.com>
2026-05-24 22:11:34 +0800

    Updated suggestion footer #42

--- resources/js/components/xeril/suggestion-footer.tsx
+++ resources/js/components/xeril/suggestion-footer.tsx
@@ -1,16 +1,27 @@
+import { insertSuggestionToSlot } from '@/routes/slots';
+import { listCustomMaps } from '@/routes/tournaments/custom';
 import { showPoolingPanel } from '@/routes/tournaments/mappools';
 import { Tournament } from '@/types/tournament';
 import { Link } from '@inertiajs/react';
 
 interface SuggestionFooterProps {
     tournament: Tournament;
-    current_id: number;
+    current_id?: number;
 }
 
-export default function SuggestionFooter({ tournament, current_id }: SuggestionFooterProps) {
+export default function SuggestionFooter({ tournament, current_id = -1 }: SuggestionFooterProps) {
     return (
         <div className="fixed bottom-0 z-2 flex h-16 w-full items-center justify-center bg-gray-300">
-            <Link className="flex h-full items-center px-2 text-xl font-semibold hover:bg-gray-400">Custom Maps</Link>
+            {current_id === -1 ? (
+                <div className="flex h-full items-center bg-gray-500 px-2 text-xl font-semibold text-white">Custom Maps</div>
+            ) : (
+                <Link
+                    href={listCustomMaps(tournament)}
+                    className="flex h-full items-center px-2 text-xl font-semibold hover:bg-gray-400"
+                >
+                    Custom Maps
+                </Link>
+            )}
             {tournament.mappools.map((mappool) =>
                 mappool.id === current_id ? (
                     <div
